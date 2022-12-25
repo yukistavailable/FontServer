6f3e7e500e38260a01da3c58abe0c0b439da9ee2
yukistavailable <yukitatsu_0817@yahoo.co.jp>
2022-12-26 02:17:54 +0900

    add thumbnail

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,47 @@
+import base64
+import os
+from PIL import Image
+from django.utils.html import format_html
 from django.contrib import admin
-from .models import BitMap
 
-# Register your models here.
+from .models import BitMap
+from config.settings import BASE_DIR
+
+BITMAP_DIR = os.getenv('BITMAP_DIR', 'main/bitmaps')
+BITMAP_PATH = os.path.join(BASE_DIR, BITMAP_DIR)
 
 
 class BitMapAdmin(admin.ModelAdmin):
-    list_display = ('id', 'kanji', 'created_at')
+    list_display = ('id', 'kanji', 'created_at', 'format_thumbnail',)
     list_display_links = ('id',)
     search_fields = ('kanji',)
     list_filter = ('kanji',)
+    fields = ('id', 'kanji', 'value', 'created_at', 'format_image',)
+    readonly_fields = ('id', 'created_at', 'format_image', 'format_thumbnail',)
+
+    def format_image(self, obj):
+        img_path = os.path.join(BITMAP_PATH, str(obj.id) + '.png')
+        # img to base64
+        if not os.path.exists(img_path):
+            return None
+
+        with open(img_path, 'rb') as f:
+            img = f.read()
+        img_base64 = base64.b64encode(img).decode('utf-8')
+        return format_html(
+            f'<img width=800 height=800 src="data:image/png;base64,{img_base64}" />')
+
+    def format_thumbnail(self, obj):
+        img_path = os.path.join(BITMAP_PATH, str(obj.id) + '.png')
+        # img to base64
+        if not os.path.exists(img_path):
+            return None
+
+        with open(img_path, 'rb') as f:
+            img = f.read()
+        img_base64 = base64.b64encode(img).decode('utf-8')
+        return format_html(
+            f'<img width=80 height=80 src="data:image/png;base64,{img_base64}" />')
 
 
 admin.site.register(BitMap, BitMapAdmin)
--- main/views/bitmap_views.py
+++ main/views/bitmap_views.py
@@ -9,6 +9,7 @@ from rest_framework import status, views
 from ..models import BitMap
 from ..serializers.bitmap_serializers import CreateRetrieveBitMapSerializer
 from config.settings import BASE_DIR
+from PIL import Image
 
 BITMAP_DIR = os.getenv('BITMAP_DIR', 'main/bitmaps')
 BITMAP_PATH = os.path.join(BASE_DIR, BITMAP_DIR)
@@ -18,15 +19,35 @@ if not os.path.exists(BITMAP_PATH):
 
 
 def save_bitmap_to_json(id, value, kanji):
-    gray_scale_values = value.split(',')
-    gray_scale_values = [int(v) for v in gray_scale_values]
+    grey_scale_values = value.split(',')
+    grey_scale_values = [int(v) for v in grey_scale_values]
     bitmap = {
         'kanji': kanji,
-        'value': gray_scale_values,
+        'value': grey_scale_values,
     }
     bitmap_path = os.path.join(BITMAP_PATH, str(id) + '.json')
     with open(bitmap_path, 'w') as f:
         json.dump(bitmap, f)
+
+
+def save_bitmap_to_image(id, value):
+    grey_scale_values = value.split(',')
+    grey_scale_values = [int(v) for v in grey_scale_values]
+
+    # calculate width
+    width = 1
+    while width ** 2 < len(grey_scale_values):
+        width += 1
+
+    # create image
+    img = Image.new('L', (width, width))
+    pixels = img.load()
+    for i in range(width):
+        for j in range(width):
+            pixels[j, i] = grey_scale_values[i * width + j]
+
+    bitmap_image_path = os.path.join(BITMAP_PATH, str(id) + '.png')
+    img.save(bitmap_image_path)
 
 
 class PostGetBitMapView(views.APIView):
@@ -48,10 +69,16 @@ class PostGetBitMapView(views.APIView):
             serializer = CreateRetrieveBitMapSerializer(data=request.data)
             if serializer.is_valid(raise_exception=True):
                 created_bit_map = serializer.save()
+
                 save_bitmap_to_json(
                     created_bit_map.id,
                     created_bit_map.value,
                     created_bit_map.kanji)
+
+                save_bitmap_to_image(
+                    created_bit_map.id,
+                    created_bit_map.value)
+
                 return Response(
                     created_bit_map.id,
                     status=status.HTTP_201_CREATED)
