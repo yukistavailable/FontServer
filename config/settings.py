490b6a533843b24f890461f5ab8a737dccd0ee2c
yukistavailable <yukitatsu_0817@yahoo.co.jp>
2022-12-25 21:39:52 +0900

    fix bug

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = 'prodo/static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
